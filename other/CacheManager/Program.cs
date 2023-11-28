95513c03b04c621154edbb23b0b26b0780188cf8
Stanislav Vysotskyi <ardenhide@gmail.com>
2023-11-28 03:35:16 +0300

    - add LazyCache example

---
+++ other/CacheManager/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace LazyCache.Example;
+
+internal static class Program
+{
+    private static void Main()
+    {
+        var manager = new CachingService();
+
+        var dogKey = Guid.NewGuid().ToString();
+        manager.Add(dogKey, "Dog");
+
+        var value = manager.GetOrAdd(dogKey, _ => "Cat");
+
+        Console.WriteLine(value); // Dog
+
+        value = manager.GetOrAdd(Guid.NewGuid().ToString(), _ => "Cat");
+
+        Console.WriteLine(value); // Cat
+    }
+}
