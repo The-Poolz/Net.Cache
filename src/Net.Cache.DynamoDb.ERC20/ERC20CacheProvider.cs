a6b47c8bb202674175d92c3b99675ba3e1c3ab43
Stanislav Vysotskyi <ardenhide@gmail.com>
2024-01-24 13:14:46 +0300

    - update `ERC20CacheProvider` code

--- src/Net.Cache.DynamoDb.ERC20/ERC20CacheProvider.cs
+++ src/Net.Cache.DynamoDb.ERC20/ERC20CacheProvider.cs
@@ -11,9 +11,8 @@ public class ERC20CacheProvider : CacheProvider<string, ERC20DynamoDbTable>
         : base(new DynamoDbStorageProvider<string, ERC20DynamoDbTable>())
     { }
 
-    public virtual ERC20DynamoDbTable GetOrAdd(string key, BigInteger chainId, ERC20Service erc20Service)
-    {
-        return GetOrAdd(key, _ => new ERC20DynamoDbTable(
+    public virtual ERC20DynamoDbTable GetOrAdd(string key, BigInteger chainId, ERC20Service erc20Service) =>
+        GetOrAdd(key, _ => new ERC20DynamoDbTable(
             chainId,
             erc20Service.ContractAddress,
             erc20Service.Name(),
@@ -21,22 +20,7 @@ public class ERC20CacheProvider : CacheProvider<string, ERC20DynamoDbTable>
             erc20Service.Decimals(),
             erc20Service.TotalSupply()
         ));
-    }
 
-    public virtual ERC20DynamoDbTable GetOrAdd(string key, BigInteger chainId, EthereumAddress contractAddress, string rpcUrl)
-    {
-        return GetOrAdd(key, _ =>
-        {
-            var erc20Service = new ERC20Service(rpcUrl, contractAddress);
-
-            return new ERC20DynamoDbTable(
-                chainId,
-                contractAddress,
-                erc20Service.Name(),
-                erc20Service.Symbol(),
-                erc20Service.Decimals(),
-                erc20Service.TotalSupply()
-            );
-        });
-    }
+    public virtual ERC20DynamoDbTable GetOrAdd(string key, BigInteger chainId, EthereumAddress contractAddress, string rpcUrl) =>
+        GetOrAdd(key, chainId, new ERC20Service(rpcUrl, contractAddress));
 }
