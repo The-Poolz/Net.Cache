397a1eae9ed728501f1d62b579a46c4216f3782f
Stanislav Vysotskyi <66203238+ArdenHide@users.noreply.github.com>
2024-01-26 16:30:06 +0300

    `Net.Cache.DynamoDb.ERC20`: Flag to update `totalSupply` (#36)
    * - update code
    
    * - blank line
    
    * - update/add tests
    
    * - add check if need to update `totalSupply`
    
    * - mark as static
    
    * - update test
    
    * - update code
    - add one more test
    
    * - remove `sealed`

---
+++ src/Net.Cache.DynamoDb.ERC20/ERC20StorageProvider.cs
@@ -0,0 +1,53 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+using Net.Cache.DynamoDb.ERC20.RPC;
+using Net.Cache.DynamoDb.ERC20.Models;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Net.Cache.DynamoDb.ERC20;
+
+public class ERC20StorageProvider : DynamoDbStorageProvider<string, ERC20DynamoDbTable>
+{
+    public ERC20StorageProvider() { }
+    public ERC20StorageProvider(IDynamoDBContext context) : base(context) { }
+
+    protected ERC20DynamoDbTable UpdateTotalSupply(ERC20DynamoDbTable existValue, IERC20Service erc20Service)
+    {
+        var updatedValue = new ERC20DynamoDbTable(
+            existValue.ChainId,
+            existValue.Address,
+            existValue.Name,
+            existValue.Symbol,
+            existValue.Decimals,
+            erc20Service.TotalSupply()
+        );
+        Context.SaveAsync(updatedValue)
+            .GetAwaiter()
+            .GetResult();
+        return updatedValue;
+    }
+
+    public bool TryGetValue(string key, GetCacheRequest request, [MaybeNullWhen(false)] out ERC20DynamoDbTable value)
+    {
+        value = default;
+        try
+        {
+            value = Context.LoadAsync<ERC20DynamoDbTable>(key)
+                .GetAwaiter()
+                .GetResult();
+
+            if (value == null)
+            {
+                return false;
+            }
+            if (request.UpdateTotalSupply)
+            {
+                value = UpdateTotalSupply(value, request.ERC20Service);
+            }
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
---
+++ src/Net.Cache.DynamoDb.ERC20/Models/GetCacheRequest.cs
@@ -0,0 +1,23 @@
+﻿using System.Numerics;
+using Net.Web3.EthereumWallet;
+using Net.Cache.DynamoDb.ERC20.RPC;
+
+namespace Net.Cache.DynamoDb.ERC20.Models;
+
+public class GetCacheRequest
+{
+    public BigInteger ChainId { get; }
+    public IERC20Service ERC20Service { get; }
+    public bool UpdateTotalSupply { get; }
+
+    public GetCacheRequest(BigInteger chainId, EthereumAddress contractAddress, string rpcUrl, bool updateTotalSupply = true)
+        : this(chainId, new ERC20Service(rpcUrl, contractAddress))
+    { }
+
+    public GetCacheRequest(BigInteger chainId, IERC20Service erc20Service, bool updateTotalSupply = true)
+    {
+        ChainId = chainId;
+        ERC20Service = erc20Service;
+        UpdateTotalSupply = updateTotalSupply;
+    }
+}
