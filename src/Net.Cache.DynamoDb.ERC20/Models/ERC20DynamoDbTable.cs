ceb9c5f224f9bf6b8b57d1f3d88cd6d1bb321f1b
Stanislav Vysotskyi <66203238+ArdenHide@users.noreply.github.com>
2024-02-06 17:08:29 +0300

    Net.Cache.DynamoDb.ERC20: Write XML docs (#45)
    * Net.Cache.DynamoDb.ERC20: Write XML docs
    
    * - multiple blank lines in a row
    
    * - beautify

--- src/Net.Cache.DynamoDb.ERC20/Models/ERC20DynamoDbTable.cs
+++ src/Net.Cache.DynamoDb.ERC20/Models/ERC20DynamoDbTable.cs
@@ -6,30 +6,71 @@ using Net.Cache.DynamoDb.ERC20.RPC;
 
 namespace Net.Cache.DynamoDb.ERC20.Models;
 
+/// <summary>
+/// Represents an ERC20 token information cache entry in DynamoDB.
+/// </summary>
+/// <remarks>
+/// This class is designed to store and manage the basic details of an ERC20 token,
+/// including its chain ID, address, name, symbol, decimals, and total supply. It also
+/// generates a unique hash key for each token based on its chain ID and address.
+/// </remarks>
 [DynamoDBTable("TokensInfoCache")]
 public class ERC20DynamoDbTable
 {
+    /// <summary>
+    /// Gets the hash key for the ERC20 token entry, uniquely generated based on the chain ID and token address.
+    /// </summary>
     [DynamoDBHashKey]
     public string HashKey { get; }
 
+    /// <summary>
+    /// Gets the block-chain chain ID where the ERC20 token is located.
+    /// </summary>
     [DynamoDBProperty]
     public BigInteger ChainId { get; }
 
+    /// <summary>
+    /// Gets the ERC20 token contract address.
+    /// </summary>
     [DynamoDBProperty]
     public string Address { get; }
 
+    /// <summary>
+    /// Gets the name of the ERC20 token.
+    /// </summary>
     [DynamoDBProperty]
     public string Name { get; }
 
+    /// <summary>
+    /// Gets the symbol of the ERC20 token.
+    /// </summary>
     [DynamoDBProperty]
     public string Symbol { get; }
 
+    /// <summary>
+    /// Gets the decimals of the ERC20 token, indicating how divisible it is.
+    /// </summary>
     [DynamoDBProperty]
     public byte Decimals { get; }
 
+    /// <summary>
+    /// Gets the total supply of the ERC20 token.
+    /// </summary>
     [DynamoDBProperty]
     public decimal TotalSupply { get; }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ERC20DynamoDbTable"/> class with specified token details.
+    /// </summary>
+    /// <param name="chainId">The block-chain chain ID.</param>
+    /// <param name="address">The ERC20 token contract address.</param>
+    /// <param name="name">The name of the ERC20 token.</param>
+    /// <param name="symbol">The symbol of the ERC20 token.</param>
+    /// <param name="decimals">The decimals of the ERC20 token.</param>
+    /// <param name="totalSupply">The total supply of the ERC20 token.</param>
+    /// <remarks>
+    /// This constructor expects an already calculated <paramref name="totalSupply"/> based on the <see cref="BigInteger"/> total supply value and <paramref name="decimals"/>.
+    /// </remarks>
     public ERC20DynamoDbTable(BigInteger chainId, EthereumAddress address, string name, string symbol, byte decimals, decimal totalSupply)
     {
         ChainId = chainId;
@@ -41,6 +82,18 @@ public class ERC20DynamoDbTable
         HashKey = $"{ChainId}-{Address}".ToSha256();
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ERC20DynamoDbTable"/> class with specified token details and total supply in <see cref="BigInteger"/>.
+    /// </summary>
+    /// <param name="chainId">The block-chain chain ID.</param>
+    /// <param name="address">The ERC20 token contract address.</param>
+    /// <param name="name">The name of the ERC20 token.</param>
+    /// <param name="symbol">The symbol of the ERC20 token.</param>
+    /// <param name="decimals">The decimals of the ERC20 token.</param>
+    /// <param name="totalSupply">The total supply of the ERC20 token in <see cref="BigInteger"/> format.</param>
+    /// <remarks>
+    /// This constructor converts the <paramref name="totalSupply"/> from <see cref="BigInteger"/> to <see langword="decimal"/>, considering the token's <paramref name="decimals"/>.
+    /// </remarks>
     public ERC20DynamoDbTable(BigInteger chainId, EthereumAddress address, string name, string symbol, byte decimals, BigInteger totalSupply)
     {
         ChainId = chainId;
@@ -52,6 +105,14 @@ public class ERC20DynamoDbTable
         HashKey = $"{ChainId}-{Address}".ToSha256();
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ERC20DynamoDbTable"/> class using an <see cref="IERC20Service"/> to populate token details.
+    /// </summary>
+    /// <param name="chainId">The block-chain chain ID.</param>
+    /// <param name="erc20Service">The ERC20 service providing access to token details.</param>
+    /// <remarks>
+    /// This constructor retrieves token details such as name, symbol, decimals, and total supply from the provided <see cref="IERC20Service"/>.
+    /// </remarks>
     public ERC20DynamoDbTable(BigInteger chainId, IERC20Service erc20Service)
     {
         ChainId = chainId;
--- src/Net.Cache.DynamoDb.ERC20/RPC/ERC20Service.cs
+++ src/Net.Cache.DynamoDb.ERC20/RPC/ERC20Service.cs
@@ -5,26 +5,55 @@ using Nethereum.Contracts.Standards.ERC20;
 
 namespace Net.Cache.DynamoDb.ERC20.RPC;
 
+/// <summary>
+/// Provides functionalities to interact with ERC20 tokens on the block-chain via RPC calls.
+/// </summary>
+/// <remarks>
+/// This class encapsulates the interactions with an ERC20 token contract,
+/// allowing for queries such as retrieving the token's name, symbol, decimals, and total supply.
+/// </remarks>
 public class ERC20Service : IERC20Service
 {
     private readonly ERC20ContractService contractService;
+
+    /// <inheritdoc cref="IERC20Service.ContractAddress"/>
     public EthereumAddress ContractAddress { get; }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ERC20Service"/> class using an RPC URL and contract address.
+    /// </summary>
+    /// <param name="rpcUrl">The URL of the RPC endpoint to interact with the block-chain.</param>
+    /// <param name="contractAddress">The ERC20 token contract address.</param>
+    /// <remarks>
+    /// This constructor initializes the contract service for interacting with the ERC20 token.
+    /// </remarks>
     public ERC20Service(string rpcUrl, EthereumAddress contractAddress)
         : this(new Nethereum.Web3.Web3(rpcUrl), contractAddress)
     { }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ERC20Service"/> class using a <see cref="Nethereum.Web3.IWeb3"/> instance and contract address.
+    /// </summary>
+    /// <param name="web3">The Web3 instance for interacting with the Ethereum network.</param>
+    /// <param name="contractAddress">The ERC20 token contract address.</param>
+    /// <remarks>
+    /// This constructor provides more flexibility by allowing the use of an existing Web3 instance.
+    /// </remarks>
     public ERC20Service(IWeb3 web3, EthereumAddress contractAddress)
     {
         ContractAddress = contractAddress;
         contractService = web3.Eth.ERC20.GetContractService(contractAddress);
     }
 
+    /// <inheritdoc cref="IERC20Service.Decimals()"/>
     public virtual byte Decimals() => contractService.DecimalsQueryAsync().GetAwaiter().GetResult();
 
+    /// <inheritdoc cref="IERC20Service.Name()"/>
     public virtual string Name() => contractService.NameQueryAsync().GetAwaiter().GetResult();
 
+    /// <inheritdoc cref="IERC20Service.Symbol()"/>
     public virtual string Symbol() => contractService.SymbolQueryAsync().GetAwaiter().GetResult();
 
+    /// <inheritdoc cref="IERC20Service.TotalSupply()"/>
     public virtual BigInteger TotalSupply() => contractService.TotalSupplyQueryAsync().GetAwaiter().GetResult();
 }
