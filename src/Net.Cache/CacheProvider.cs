43153da118df46b3be16f82b439bde1c6c1728b1
Stanislav Vysotskyi <66203238+ArdenHide@users.noreply.github.com>
2024-03-17 18:10:32 +0300

    Create interface `ICacheProvider` (#65)

--- src/Net.Cache/CacheProvider.cs
+++ src/Net.Cache/CacheProvider.cs
@@ -32,26 +32,13 @@ public class CacheProvider<TKey, TValue>
         this.storageProviders = storageProviders.ToList();
     }
 
-    /// <summary>
-    /// Retrieves a value by key, or adds it to the cache using a parameter-less factory function if it's not already present.
-    /// This method is useful when the creation of the value does not require any parameters.
-    /// </summary>
-    /// <param name="key">The key for retrieving or adding the value.</param>
-    /// <param name="valueFactory">A function that creates a value when required. It takes no parameters.</param>
-    /// <returns>The cached or newly added value associated with the specified <paramref name="key"/>.</returns>
+    /// <inheritdoc cref="ICacheProvider{TKey, TValue}.GetOrAdd(TKey, Func{TValue})"/>
     public virtual TValue GetOrAdd(TKey key, Func<TValue> valueFactory)
     {
         return GetOrAddInternal(key, _ => valueFactory());
     }
 
-    /// <summary>
-    /// Retrieves a value by key, or adds it to the cache using a factory function with parameters if it's not already present.
-    /// This method allows for more complex value creation scenarios where parameters are needed.
-    /// </summary>
-    /// <param name="key">The key for retrieving or adding the value.</param>
-    /// <param name="valueFactory">A function that creates a value when required. This function can take any number of parameters.</param>
-    /// <param name="args">The arguments to pass to the <paramref name="valueFactory"/> function.</param>
-    /// <returns>The cached or newly added value associated with the specified <paramref name="key"/>.</returns>
+    /// <inheritdoc cref="ICacheProvider{TKey, TValue}.GetOrAdd(TKey, Func{object[], TValue}, object[])"/>
     public virtual TValue GetOrAdd(TKey key, Func<object[], TValue> valueFactory, params object[] args)
     {
         return GetOrAddInternal(key, valueFactory, args);
