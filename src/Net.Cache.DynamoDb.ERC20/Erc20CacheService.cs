c454de0fae9f03f9553237896a5f4ab2b2b6a9bd
Stanislav Vysotskyi <66203238+ArdenHide@users.noreply.github.com>
2025-09-25 10:43:29 +0300

    Net.Cache.DynamoDb.ERC20: Overload with IWeb3 (#139)
    * Net.Cache.DynamoDb.ERC20: Overload with IWeb3
    
    * - update README

--- src/Net.Cache.DynamoDb.ERC20/Erc20CacheService.cs
+++ src/Net.Cache.DynamoDb.ERC20/Erc20CacheService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Nethereum.Web3;
 using System.Threading.Tasks;
 using Net.Web3.EthereumWallet;
 using Net.Cache.DynamoDb.ERC20.Rpc;
@@ -39,11 +40,27 @@ namespace Net.Cache.DynamoDb.ERC20
             )
         { }
 
-        /// <inheritdoc cref="IErc20CacheService.GetOrAddAsync"/>
-        public async Task<Erc20TokenDynamoDbEntry> GetOrAddAsync(HashKey hashKey, Func<Task<string>> rpcUrlFactory, Func<Task<EthereumAddress>> multiCallFactory)
+        /// <inheritdoc cref="IErc20CacheService.GetOrAddAsync(HashKey, Func{Task{string}}, Func{Task{EthereumAddress}})"/>
+        public Task<Erc20TokenDynamoDbEntry> GetOrAddAsync(HashKey hashKey, Func<Task<string>> rpcUrlFactory, Func<Task<EthereumAddress>> multiCallFactory)
+        {
+            if (rpcUrlFactory == null) throw new ArgumentNullException(nameof(rpcUrlFactory));
+
+            return GetOrAddAsync(
+                hashKey,
+                async () =>
+                {
+                    var rpcUrl = await rpcUrlFactory().ConfigureAwait(false);
+                    return new Nethereum.Web3.Web3(rpcUrl);
+                },
+                multiCallFactory
+            );
+        }
+
+        /// <inheritdoc cref="IErc20CacheService.GetOrAddAsync(HashKey, Func{Task{IWeb3}}, Func{Task{EthereumAddress}})"/>
+        public async Task<Erc20TokenDynamoDbEntry> GetOrAddAsync(HashKey hashKey, Func<Task<IWeb3>> web3Factory, Func<Task<EthereumAddress>> multiCallFactory)
         {
             if (hashKey == null) throw new ArgumentNullException(nameof(hashKey));
-            if (rpcUrlFactory == null) throw new ArgumentNullException(nameof(rpcUrlFactory));
+            if (web3Factory == null) throw new ArgumentNullException(nameof(web3Factory));
             if (multiCallFactory == null) throw new ArgumentNullException(nameof(multiCallFactory));
 
             if (_inMemoryCache.TryGetValue(hashKey.Value, out var cachedEntry)) return cachedEntry;
@@ -57,15 +74,15 @@ namespace Net.Cache.DynamoDb.ERC20
                 return entry;
             }
 
-            var rpcUrlTask = rpcUrlFactory();
+            var web3Task = web3Factory();
             var multiCallTask = multiCallFactory();
 
-            await Task.WhenAll(rpcUrlTask, multiCallTask);
+            await Task.WhenAll(web3Task, multiCallTask).ConfigureAwait(false);
 
-            var rpcUrl = rpcUrlTask.Result;
+            var web3 = web3Task.Result;
             var multiCall = multiCallTask.Result;
 
-            var erc20Service = _erc20ServiceFactory.Create(new Nethereum.Web3.Web3(rpcUrl), multiCall);
+            var erc20Service = _erc20ServiceFactory.Create(web3, multiCall);
             var erc20Token = await erc20Service.GetErc20TokenAsync(hashKey.Address).ConfigureAwait(false);
 
             entry = new Erc20TokenDynamoDbEntry(hashKey, erc20Token);
